8bd7732076d880c015042352081704b6d133e602
Chris L <chris.ladden@gmail.com>
2022-11-23 12:50:40 -0600

    built authentication react app using Firebase

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import SignUp from './Signup';
+import Dashboard from './Dashboard';
+import UpdateProfile from './UpdateProfile';
+import Login from './Login';
+import ForgotPassword from './ForgotPassword';
+import { Container } from 'react-bootstrap'
+import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from '../contexts/PrivateRoute';
+
+function App() {
+  return (
+      <Container className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: '400px' }}>
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route exact path="/" element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                  }
+                />
+                <Route exact path="/update-profile" element={
+                  <PrivateRoute>
+                    <UpdateProfile />
+                  </PrivateRoute>
+                  }
+                />
+                <Route path="/signup" element={<SignUp />} /> 
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+              </Routes>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+  )
+}
+
+export default App;
